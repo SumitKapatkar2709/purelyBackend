9e0208f4c554b89ea07e44e59776cd0138b88ff0
SumitKapatkar2709 <sumitkapatkar3@gmail.com>
2025-04-08 23:58:50 +0530

    Revert "Merge pull request #1 from SumitKapatkar2709/PaymentBranch"
    This reverts commit 5d46c8fda04788d0d49b86809355d421c5ae4709, reversing
    changes made to 43118d4cbec87ba009bf917bef956066d089295e.

--- microservice-backend/cart-service/src/main/java/com/dharshi/cartservice/exceptions/PaymentException.java
+++
@@ -1,7 +0,0 @@
-package com.dharshi.cartservice.exceptions;
-
-public class PaymentException extends RuntimeException {
-    public PaymentException(String message) {
-        super(message);
-    }
-}
